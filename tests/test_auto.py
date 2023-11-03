d4139b4a4d56af8b8c7d9351e148a42df0862e57
Vladimir Lipkin <lipkin@yandex-team.ru>
2023-11-03 16:49:46 +0300

    Add defaults to AutoExplorer

---
+++ tests/test_auto.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import sys
+from envzy import AutoExplorer
+
+
+def test_defaults(pypi_index_url):
+    # NB: I'm not sure if it have any sense to do such test...
+    # but at least I checking possibility to instantiate
+    # explorer without any arguments
+    explorer = AutoExplorer()
+
+    assert explorer.additional_pypi_packages == {}
+    assert explorer.pypi_index_url == pypi_index_url
+    assert explorer.target_python == sys.version_info[:2]
