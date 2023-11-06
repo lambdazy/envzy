a9978bd544fa156fbcf83345ffc2948f487aa0c5
Vladimir Lipkin <lipkin@yandex-team.ru>
2023-11-06 19:38:46 +0300

    Add some things into API to use in pylzy

--- envzy/__init__.py
+++ envzy/__init__.py
@@ -1,8 +1,14 @@
-from .base import ModulePathsList, PackagesDict
 from .auto import AutoExplorer
+from .base import ModulePathsList, PackagesDict, BaseExplorer
+from .exceptions import BadPypiIndex
+from .pypi import PYPI_INDEX_URL_DEFAULT, validate_pypi_index_url
 
 __all__ = [
     'AutoExplorer',
+    'BaseExplorer',
     'ModulePathsList',
     'PackagesDict',
+    'BadPypiIndex',
+    'PYPI_INDEX_URL_DEFAULT',
+    'validate_pypi_index_url',
 ]
